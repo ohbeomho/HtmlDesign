ddc55df1f2f5cda4e985616e0736ae6a3cbf6d49
OhBeomho <tyler0322@naver.com>
2022-05-04 16:47:52 +0900

    blog

--- blogDesign/script.js
+++ blogDesign/script.js
@@ -3,8 +3,6 @@ const writeComment = document.querySelector(".write-comment");
 const commentButton = document.querySelector(".comment-button");
 const commentText = document.querySelector(".comment-text");
 
-const username = "GUEST";
-
 addComment.addEventListener("click", () => {
 	if (writeComment.getAttribute("style")) {
 		writeComment.removeAttribute("style");	
@@ -14,27 +12,9 @@ addComment.addEventListener("click", () => {
 });
 
 commentButton.addEventListener("click", () => {
-	write();
-
 	commentText.value = "";
 	writeComment.setAttribute("style", "display: none;");
 });
-
-function write() {
-	let comment = document.createElement("div");
-	comment.className = "comment";
-	let date = new Date();
-	let dateString = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDay()}`;
-	comment.innerHTML = `
-		<div class="info">
-			<div class="writer">${username}</div>
-			<div class="date">${dateString}</div>
-		</div>
-		<div class="text">
-			${commentText.value}
-		</div>`;
-	document.querySelector(".comments").appendChild(comment);
-}
 
 function resizeTA() {
 	commentText.style.height = "auto";
