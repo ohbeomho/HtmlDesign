542d0d8add60200fdd6b68c27861af4429c7ec1d
OhBeomho <tyler0322@naver.com>
2022-05-05 09:58:48 +0900

    blog

--- blogDesign/script.js
+++ blogDesign/script.js
@@ -3,6 +3,10 @@ const writeComment = $(".write-comment");
 const commentButton = $(".comment-button");
 const commentText = $(".comment-text");
 
+const profileButton = $(".items #profile");
+const profileModal = $(".profile-modal");
+const closeProfileButton = $(".profile-modal #close");
+
 addComment.on("click", () => {
 	if (writeComment.css("display") == "none") {
 		writeComment.css("display", "block");
@@ -14,6 +18,16 @@ addComment.on("click", () => {
 commentButton.on("click", () => {
 	commentText.value = "";
 	writeComment.css("display", "none");
+});
+
+profileButton.on("click", () => {
+	profileModal.css("display", "block");
+	$("body").css("overflow-y", "hidden");
+});
+
+closeProfileButton.on("click", () => {
+	profileModal.css("display", "none");
+	$("body").css("overflow-y", "scroll");
 });
 
 function resizeTA() {
