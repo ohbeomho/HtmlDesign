4ca46b271ca29a7406f16ea54c0998cad2540ea8
OhBeomho <tyler0322@naver.com>
2022-05-10 20:25:04 +0900

    search things

--- searchDesign/result.js
+++ searchDesign/result.js
@@ -1,18 +1,42 @@
-const searchText = $(".search-text");
-const searchButton = $(".search-button");
+const searchText = $(".search-text")
+const searchButton = $(".search-button")
 
 $(document).ready(() => {
-	let search = getQS().search;
+    searchButton.on("click", () => {
+        let path = location.href.split("?")[0]
+    
+        if (searchText.val() == "") {
+            return
+        }
+    
+        location.href = `${path}?search=${searchText.val()}`
+    })
+    
+    searchText.on("keydown", (event) => {
+        if (event.keyCode == 13) {
+            let path = location.href.split("?")[0]
+    
+            if (searchText.val() == "") {
+                return
+            }
+    
+            location.href = `${path}?search=${searchText.val()}`
+        }
+    })
 
-
-});
+	let search = getQS().search
+    searchText.val(search)
+    searchText.focus()
+})
 
 function getQS() {
-	var url = document.location.href;
-	var qs = url.substring(url.indexOf('?') + 1).split('&');
-	for(var i = 0, result = {}; i < qs.length; i++) {
-		qs[i] = qs[i].split('='); result[qs[i][0]] = decodeURIComponent(qs[i][1]);
+	let url = document.location.href
+	let qs = url.substring(url.indexOf("?") + 1).split("&")
+    let result = {}
+	for(let i = 0; i < qs.length; i++) {
+		qs[i] = qs[i].split("=")
+        result[qs[i][0]] = decodeURIComponent(qs[i][1])
 	}
 
-	return result;
+	return result
 }
