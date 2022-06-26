29e752ae0767d4ec6d1fbdf624b660fb31e095ba
OhBeomho <tyler0322@naver.com>
2022-06-26 14:10:02 +0900

    new design: mail

---
+++ mailDesign/js/mail_data.js
@@ -0,0 +1,14 @@
+[
+    {
+        "subject": "Hey!",
+        "writer": "MyFriend",
+    },
+    {
+        "subject": "Please verify your device.",
+        "writer": "WebsiteName",
+    },
+    {
+        "subject": "New message from ChatApp",
+        "writer": "ChatApp"
+    }
+]
