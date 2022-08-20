2f73b6850e4eb84b695176b7ca225266c67b28af
OhBeomho <tyler0322@naver.com>
2022-08-20 20:21:47 +0900

    finish

--- blogDesign/script.js
+++ blogDesign/script.js
@@ -1,1 +1,67 @@
-// TODO: Code here when html, css is done.
+function addComment(id, name, text) {
+	const commentDOM = `<div class="profile">
+		<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="">
+		<div class="name">${name}</div>
+	</div>
+	<div class="content">
+		<div class="text">${text}</div>
+		<div class="buttons">
+			<button class="like">
+				<i class="fas fa-heart"></i>
+				Like
+			</button>
+			<button class="unlike">
+				<i class="fas fa-thumbs-down"></i>
+				Unlike
+			</button>
+		</div>
+	</div>`;
+	const commentElement = document.createElement('div');
+	commentElement.className = 'comment';
+	commentElement.innerHTML = commentDOM;
+	document.getElementsByClassName('comment-write')[id - 1].before(commentElement);
+
+	const like = commentElement.getElementsByClassName('like')[0];
+	const unlike = commentElement.getElementsByClassName('unlike')[0];
+
+	like.addEventListener('click', () => {
+		like.classList.toggle('active');
+		like.nextElementSibling.classList.remove('active');
+	});
+
+	unlike.addEventListener('click', () => {
+		unlike.classList.toggle('active');
+		unlike.previousElementSibling.classList.remove('active');
+	});
+}
+
+Array.from(document.getElementsByClassName('blog')).forEach((e) => {
+	e.getElementsByClassName('add-comment')[0].addEventListener('click', () => {
+		const name = 'Blogger';
+		const text = e.getElementsByClassName('comment-content')[0];
+
+		if (text.value) {
+			addComment(Number(e.id), name, text.value);
+			text.value = '';
+		}
+	});
+});
+
+Array.from(document.getElementsByClassName('like')).forEach((e) => {
+	e.addEventListener('click', () => {
+		e.classList.toggle('active');
+		e.nextElementSibling.classList.remove('active');
+	});
+});
+
+Array.from(document.getElementsByClassName('unlike')).forEach((e) => {
+	e.addEventListener('click', () => {
+		e.classList.toggle('active');
+		e.previousElementSibling.classList.remove('active');
+	});
+});
+
+Array.from(document.getElementsByClassName('write-time')).forEach((e) => {
+	const date = new Date();
+	e.innerText = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+});
